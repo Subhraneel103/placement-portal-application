57d160c6ef715b5f558513a97809bcb249d3f932
Subhraneel Bhadury <24f3003262@ds.study.iitm.ac.in>
2026-04-08 00:17:49 +0530

    add several functionalities for admin student and company like viewing accept rejecting applications, seeing placement history etc along with some UI design changes

--- application/controllers.py
+++ application/controllers.py
@@ -5,6 +5,8 @@ import os
 from .models import *
 from datetime import datetime
 
+from flask import jsonify
+
 #Upload storage
 Upload_Folder='static/uploads/resumes'
 Allowed_Extensions=['pdf','docx','pptx']
@@ -78,6 +80,7 @@ def login():
 
 @app.route('/register/student',methods=['GET','POST'])
 def register_student():
+    
     if request.method=="POST":
         # taking data from html
         email=request.form.get('email')
@@ -178,21 +181,65 @@ def admin_dashboard():
     all_students=Student.query.all()
     all_companies=Company.query.all()
     pending_drives=PlacementDrive.query.filter_by(status='Pending').all()
-    
+    active_drives=PlacementDrive.query.filter(PlacementDrive.status.in_(['Approved','Stopped'])).all()
+
 
     # Statistics
     stats = {
         'students': Student.query.count(),
-        'companies': Company.query.count(),
-        'drives': PlacementDrive.query.count(),
+        'companies': Company.query.filter_by(is_approved=True).count(),
+        'drives': PlacementDrive.query.filter_by(status='Approved').count(),
         'applications': Application.query.count()
     }
+
+
+    ## SEARCH
+
+    s_q=request.args.get('student_q','')
+    c_q=request.args.get('company_q','')
+    
+    student_query=Student.query
+    if s_q:
+        if s_q.isdigit():
+            student_query=student_query.filter(Student.id==s_q)
+        else:
+            student_query=student_query.filter((Student.name.ilike(f'%{s_q}%')) |
+                                                (Student.user.has(User.username).ilike(f'%{s_q}%')) | 
+                                                (Student.user.has(User.email).ilike(f'%{s_q}%')) | 
+                                                (Student.college.ilike(f'%{s_q}%')))
+    
+    all_students = student_query.all()
+    company_query=Company.query.filter_by(is_approved=True)
+    if c_q:
+        if c_q.isdigit():
+            company_query=company_query.filter(Company.id==c_q)
+        else:
+            company_query=company_query.filter((Company.company_name.ilike(f'%{c_q}%')) |
+                                                (Company.user.has(User.username).ilike(f'%{c_q}%'))|
+                                                (Company.user.has(User.email).ilike(f'%{c_q}%'))|
+                                                (Company.hr_name.ilike(f'%{c_q}%')))
+        
+    all_companies = company_query.all()
+
+    d_q = request.args.get('drive_q', '')
+    drive_query = PlacementDrive.query.filter(PlacementDrive.status.in_(['Approved', 'Stopped']))
+    if d_q:
+        drive_query = drive_query.filter(PlacementDrive.job_title.ilike(f'%{d_q}%') | 
+                                         PlacementDrive.allowed_branches.ilike(f'%{d_q}%')|
+                                         PlacementDrive.allowed_years.ilike(f'%{d_q}%')|
+                                         PlacementDrive.company.has(Company.company_name.ilike(f'%{d_q}%')))
+    active_drives = drive_query.all()
+
+
+
     return render_template('admin_dashboard.html',
                            stats=stats,
                            pending_companies=pending_companies,
                            students=all_students,
                            all_companies=all_companies,
-                           pending_drives=pending_drives)
+                           pending_drives=pending_drives,
+                           active_drives=active_drives,
+                           s_q=s_q,c_q=c_q,d_q=d_q)
 
 
 
@@ -238,6 +285,68 @@ def company_dashboard():
 
     return render_template('company_dashboard.html',company=company,drives=drives,active_count=active_drive,pending_count=pending_drive,total_applications=total_applications)
 
+
+#Stop a drive
+@app.route('/company/stop_drive/<int:drive_id>',methods=['POST'])
+def company_stop_drive(drive_id):
+    drive=PlacementDrive.query.get_or_404(drive_id)
+
+    #Security: Company ka hi drive hona chahiye
+    if drive.company.user_id!=session.get('user_id'):
+        flash("Unauthorized! Must use company account",'danger')
+        return "Unauthorized",403
+    
+    drive.status='Stopped'
+    db.session.commit()
+    flash(f"Drive '{drive.job_title}' has been stopped.","success")
+    return redirect(url_for('company_dashboard'))
+
+#Route to resume a drive (company side)
+@app.route('/company/resume_drive/<int:drive_id>',methods=['POST'])
+def company_resume_drive(drive_id):
+    drive=PlacementDrive.query.get_or_404(drive_id)
+    if drive.company.user_id!=session.get('user_id'):
+        flash("Unauthorized! Must use company account",'danger')
+        return "Unauthorized",403
+    drive.status = 'Approved' # Or 'Pending' if you want Admin to re-verify
+    db.session.commit()
+    flash(f"Drive '{drive.job_title}' has been resumed.", "success")
+    return redirect(url_for('company_dashboard'))
+
+@app.route('/company/edit-drive/<int:drive_id>',methods=['GET','POST'])
+def edit_drive(drive_id):
+    if session.get('role')!='company':
+        return redirect(url_for('login'))
+    
+    drive=PlacementDrive.query.get_or_404(drive_id)
+    company=Company.query.filter_by(user_id=session['user_id']).first()
+
+    if drive.company_id!=company.id:
+        flash("Unauthorized access.",'danger')
+        return redirect(url_for('company_dashboard'))
+    
+    if request.method=="POST":
+        drive.job_title=request.form.get('job_title')
+        drive.job_description=request.form.get('job_description')
+        drive.job_location=request.form.get('job_location')
+        drive.eligibility_CGPA=request.form.get('eligibility_CGPA')
+        drive.eligibility_criteria_others=request.form.get('eligibility_others')
+        drive.allowed_branches=request.form.get('allowed_branches')
+        drive.allowed_years=request.form.get('allowed_years')
+
+        deadline_str=request.form.get('deadline')
+        if deadline_str:
+            drive.deadline=datetime.strptime(deadline_str,'%Y-%m-%d')
+        
+        db.session.commit()
+        flash("Drive updated successfully",'success')
+        return redirect(url_for('company_dashboard'))
+    
+    return render_template('edit_drive.html',drive=drive)
+
+
+
+    
 
 
 ## ADMIN METHODSS
@@ -361,6 +470,53 @@ def reject_drive(drive_id):
     flash(f'Drive "{drive.job_title}" rejected.','success')
     return redirect(url_for('admin_dashboard'))
 
+@app.route('/admin/review_drive/<int:drive_id>')
+def admin_review_drive(drive_id):
+    if session.get('role') != 'admin':
+        return redirect(url_for('login'))
+
+    drive = PlacementDrive.query.get_or_404(drive_id)
+    return render_template('admin_review_drive.html', drive=drive)
+
+
+@app.route('/admin/stop_drive/<int:drive_id>', methods=['POST'])
+def stop_drive(drive_id):
+    if session.get('role') != 'admin':
+        return redirect(url_for('login'))
+    
+    drive = PlacementDrive.query.get_or_404(drive_id)
+    drive.status = 'Stopped'  # This removes it from the Student's "Active" list
+    db.session.commit()
+    flash(f"Placement Drive '{drive.job_title}' has been stopped.", "warning")
+    return redirect(url_for('admin_dashboard'))
+
+@app.route('/admin/delete_drive/<int:drive_id>', methods=['POST'])
+def delete_drive(drive_id):
+    if session.get('role') != 'admin':
+        return redirect(url_for('login'))
+    
+    drive = PlacementDrive.query.get_or_404(drive_id)
+    db.session.delete(drive)
+    db.session.commit()
+    flash("Drive deleted from the system.", "danger")
+    return redirect(url_for('admin_dashboard'))
+
+@app.route('/admin/resume_drive/<int:drive_id>', methods=['POST'])
+def resume_drive(drive_id):
+    if session.get('role') != 'admin':
+        return redirect(url_for('login'))
+    
+    drive = PlacementDrive.query.get_or_404(drive_id)
+    
+    #status back to Approved to make it visible to students again
+    drive.status = 'Approved'
+    db.session.commit()
+    
+    flash(f"Placement Drive '{drive.job_title}' has been resumed and is now live.", "success")
+    return redirect(url_for('admin_dashboard'))
+
+
+### STUDENT METHODS
 
 @app.route('/student/edit-profile',methods=['GET','POST'])
 def edit_profile():
@@ -388,6 +544,20 @@ def edit_profile():
         return redirect(url_for('student_dashboard'))
     
     return render_template('edit_profile_student.html',student=student)
+
+@app.route('/student/history')
+def student_history():
+    # Auth
+    if session.get('role')!='student':
+        flash('Login as student','danger')
+        return redirect(url_for('login'))
+    # Stud obj
+    student=Student.query.filter_by(user_id=session.get('user_id')).first_or_404()
+    # ordered by ID descending-> show most recent appl first.
+    applications=Application.query.filter_by(student_id=student.id).order_by(Application.id.desc()).all()
+
+    return render_template('student_history.html',student=student,applications=applications)
+
     
 @app.route('/drive/<int:drive_id>/details')
 def view_drive(drive_id):
@@ -427,22 +597,6 @@ def apply_drive(drive_id):
     return redirect(url_for('view_drive',drive_id=drive.id))
 
 
-
-    
-
-@app.route('/student/history')
-def application_history():
-    # Placeholder for now
-    if session.get('role')!='student':
-        flash('Login as student','danger')
-        return redirect(url_for('login'))
-    
-    student=Student.query.filter_by(user_id=session['user_id']).first()
-    applications=Application.query.filter_by(student_id=student.id).all()
-
-    return render_template('application_history.html',student=student,applications=applications)
-
-
 @app.route('/company/<int:company_id>/details')
 def view_company(company_id):
     # This matches the 'View Openings' link
@@ -457,11 +611,6 @@ def view_company(company_id):
     return render_template('view_company.html',company=company,drives=activeDrives)
 
 
-
-
-
-
-# controllers.py
 
 @app.route('/company/drive/<int:drive_id>/applicants',methods=['GET','POST'])
 def view_applicants(drive_id):
@@ -526,7 +675,7 @@ def create_drive():
         job_description=request.form.get('job_description')
         eligibility_CGPA=request.form.get('eligibility_CGPA')
         job_location=request.form.get('job_location')
-        eligibility_others=request.form.get('eligibility_others')
+        eligibility_others=request.form.get('eligibility_criteria_others')
         deadline=request.form.get('deadline')
         allowed_branches = request.form.get('allowed_branches')
         allowed_years = request.form.get('allowed_years')
@@ -586,3 +735,29 @@ def admin_approve_company(company_id):
 
     flash(f"Company '{company.company_name}' has been approved",'success')
     return redirect(url_for('admin_approvals'))
+
+
+@app.route('/admin/history/')
+def admin_history():
+    if session.get('role')!='admin':
+        return redirect(url_for('login'))
+    
+    # Searching ka query
+    q=request.args.get('q','')
+    
+    # Historical data uthana(descending)
+    drive_query=PlacementDrive.query.order_by(PlacementDrive.created_at.desc())
+    if q:
+        drive_query=drive_query.filter(PlacementDrive.job_title.ilike(f'%{q}%'))
+    historical_drives=drive_query.all()
+
+    #jo jo select huye
+    successful_placements=Application.query.filter_by(status='Shortlisted').all()
+
+    # kitne applications aur kitne success
+    total_applications=Application.query.count()
+    placed_count=len(successful_placements)
+    success_rate=((placed_count/total_applications)*100) if total_applications>0 else 0
+
+    return render_template('admin_history.html',drives=historical_drives,placements=successful_placements,q=q,placed_count=placed_count,rate=round(success_rate,2))
+
